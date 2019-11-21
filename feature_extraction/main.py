371389d7f8317f6481983b5ffe11973e94f5e427
ZijianWang <zijian.wang93@gmail.com>
2019-11-20 20:37:05 -0800

    merge data processing

--- feature_extraction/main.py
+++ feature_extraction/main.py
@@ -2,21 +2,21 @@ import model
 import utils
 
 def main:
-    num_dev_data = 50
-    num_training = 49
-    num_validation = num_dev_data - num_training
-    num_test = 10
+    dataFilePath = ''
+    labelFilePath = ''
+    testset_ratio = 0.15
+    validset_ratio = 0.02
 
-    # TODO: data processing
-    data = prepocessing()
+    data_manager = Preprocessing(dataFilePath, labelFilePath, testset_ratio, validset_ratio)
 
-    X_train, Y_train = get_train_data(data)
-    X_val, Y_val = get_val_data(data)
-    X_test, Y_test = get_test_data(data)
+    X_train, Y_train = data_manager.get_train_data()
+    X_val, Y_val = data_manager.get_val_data()
+    X_test, Y_test = data_manager.get_test_data()
 
+    # Example param for model
     model_params = ParamDict(
         model = 'default',               # use 'default' to init default model
-        input_shape = (100, 100, 3),     # (length, width, channel)
+        input_shape = (256, 256, 3),     # (length, width, channel)
         lr = 5e-4,
         step_rate = 500,
         decay_rate = 0.96,
