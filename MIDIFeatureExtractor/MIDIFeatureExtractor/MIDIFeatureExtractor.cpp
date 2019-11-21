3f7b0c9b82ddc4efaf0948f76bce06d61790d2cc
qinziang <qinziang@hujiang.com>
2019-11-20 20:10:54 -0800

    add batch I/O

--- MIDIFeatureExtractor/MIDIFeatureExtractor/MIDIFeatureExtractor.cpp
+++ MIDIFeatureExtractor/MIDIFeatureExtractor/MIDIFeatureExtractor.cpp
@@ -7,24 +7,74 @@
 #include <iostream>
 #include <iomanip>
 
+#include <iostream>
+#include "fstream"
+#include "string"
+#include <io.h>   
+#include <vector>  
+#include <algorithm>
+
+
 using namespace std;
 using namespace smf;
 
-int main(int argc, char** argv) {
-	Options options;
-	options.process(argc, argv);
+void getFiles(std::string folderPath, vector<string>& files) {
+	// refer: https://www.cnblogs.com/fnlingnzb-learner/p/6424563.html
+	// file handel
+	long hFile = 0;
+	// file info
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(folderPath).append("\\*").c_str(), &fileinfo)) != -1) {
+		do {
+			if ((fileinfo.attrib &  _A_SUBDIR)) {
+				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+					getFiles(p.assign(folderPath).append("\\").append(fileinfo.name), files);
+			}
+			else {
+				files.push_back(p.assign(folderPath).append("\\").append(fileinfo.name));
+			}
+		
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+	}
+}
+
+vector<string> getMIDIFiles(std::string folderPath, bool is_print, bool is_write) {
+	vector<string> files;
+	getFiles(folderPath, files);
+
+	if (is_print)
+		for (int i = 0; i < files.size(); i++) {
+			cout << files[i].c_str() << endl;
+		}
+	if (is_write) {
+		std::ofstream fout;
+		fout.open(folderPath + "\\midifile_names.txt");
+
+		for (int i = 0; i < files.size(); i++)
+			fout << std::setprecision(10) << files.at(i) << std::endl;
+
+		fout.close();
+	}
+
+	return files;
+}
+
+int extractFeaturesFromMIDI(std::string fileName) {
+	// parse midi file
 	MidiFile midifile;
-	if (options.getArgCount() == 0) midifile.read(cin);
-	else midifile.read(options.getArg(1));
-	// cout << options.getArg(1) << "  " << midifile.read(options.getArg(1)) << endl;
+	midifile.read(fileName);
 	midifile.doTimeAnalysis();
 	midifile.linkNotePairs();
-
-
+	
+	// extract features
+	cout << "file_name: " << fileName << endl;
+	// print hard-coded meta info
 	int controlTracks = 0;
 	int nn = 4;
 	int dd = 4;
-
+	// tse
 	int tracks = midifile.getTrackCount();
 	for (int track = 0; track < tracks; track++) {
 		if (midifile[track].size() > 2) {
@@ -41,11 +91,34 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
+	cout << "    tse: " << nn << "/" << dd << endl;
+	// bpm
+	cout << "    bpm: " << (60 * 1000000) / (midifile.getTicksPerQuarterNote() * 4) << endl;
+	// energy
+	cout << "    energy: " << controlTracks << endl;
+	
+	// return
+	return controlTracks;
+}
 
-	// print hard-coded meta info
-	cout << "bpm: " << (60 * 1000000) / (midifile.getTicksPerQuarterNote() * 4) << endl;
-	cout << "energy: " << controlTracks << endl;
-	cout << "tse: " << nn << "/" << dd << endl;
+int main(int argc, char** argv) {
+	// batch read midi files
+	std::string folderPath = "C:\\Users\\ziang\\OneDrive\\work_space\\CSCI-566\\Project\\CSCI566_GMG_Project\\MIDIFeatureExtractor\\MIDIFeatureExtractor\\midi";
+	std::vector<string> files = getMIDIFiles(folderPath, false, true);
+	sort(files.begin(), files.end());
+
+	// extract features from midi fiels
+	std::vector<int> energyList;
+	for (int i = 0; i < files.size(); i++) {
+		energyList.push_back(extractFeaturesFromMIDI(files.at(i)));
+	}
+
+	// write file
+	std::ofstream fout;
+	fout.open(folderPath + "\\energt_list.txt");
+	for (int i = 0; i < energyList.size(); i++)
+		fout << std::setprecision(10) << energyList.at(i) << std::endl;
+	fout.close();
 
 	return 0;
 }
