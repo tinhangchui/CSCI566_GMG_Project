f81879887614dec68e689939c27ef184d9570dee
qinziang <qinziang@hujiang.com>
2019-11-20 18:41:35 -0800

    parsed bpm/energy/tse

--- MIDIFeatureExtractor/MIDIFeatureExtractor/MIDIFeatureExtractor.cpp
+++ MIDIFeatureExtractor/MIDIFeatureExtractor/MIDIFeatureExtractor.cpp
@@ -16,28 +16,36 @@ int main(int argc, char** argv) {
 	MidiFile midifile;
 	if (options.getArgCount() == 0) midifile.read(cin);
 	else midifile.read(options.getArg(1));
-	cout << options.getArg(1) << "  " << midifile.read(options.getArg(1)) << "\n";
+	// cout << options.getArg(1) << "  " << midifile.read(options.getArg(1)) << endl;
 	midifile.doTimeAnalysis();
 	midifile.linkNotePairs();
 
+
+	int controlTracks = 0;
+	int nn = 4;
+	int dd = 4;
+
 	int tracks = midifile.getTrackCount();
-	cout << "TPQ: " << midifile.getTicksPerQuarterNote() << endl;
-	if (tracks > 1) cout << "TRACKS: " << tracks << endl;
 	for (int track = 0; track < tracks; track++) {
-		if (tracks > 1) cout << "\nTrack " << track << endl;
-		cout << "Tick\tSeconds\tDur\tMessage" << endl;
-		for (int event = 0; event < midifile[track].size(); event++) {
-			cout << dec << midifile[track][event].tick;
-			cout << '\t' << dec << midifile[track][event].seconds;
-			cout << '\t';
-			if (midifile[track][event].isNoteOn())
-				cout << midifile[track][event].getDurationInSeconds();
-			cout << '\t' << hex;
-			for (int i = 0; i < midifile[track][event].size(); i++)
-				cout << (int)midifile[track][event][i] << ' ';
-			cout << endl;
+		if (midifile[track].size() > 2) {
+			controlTracks++;
+		}
+		if (track == 0) {
+			for (int event = 0; event < midifile[track].size(); event++) {
+				for (int i = 0; i < midifile[track][event].size(); i++) {
+					if ((int)midifile[track][event][0] == 0xff && (int)midifile[track][event][1] == 0x58) {
+						nn = (int)midifile[track][event][2];
+						dd = (int)pow(2, (int)midifile[track][event][3]);
+					}
+				}
+			}
 		}
 	}
+
+	// print hard-coded meta info
+	cout << "bpm: " << (60 * 1000000) / (midifile.getTicksPerQuarterNote() * 4) << endl;
+	cout << "energy: " << controlTracks << endl;
+	cout << "tse: " << nn << "/" << dd << endl;
 
 	return 0;
 }
